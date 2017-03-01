87a6286da8d717a7ad47c9cc0c197d16b3684283
Oussema <o.oulhaci@gmail.com>
2017-03-01 10:41:46 +0100

    update

---
+++ src/algo/Glouton.java
@@ -0,0 +1,14 @@
+package algo;
+
+/**
+ * Created by OOussema on 28/02/2017.
+ */
+public class Glouton {
+	public int[][] matrice = {{},{}};
+
+	public Glouton(int[][] matrice) {
+		this.matrice = matrice;
+	}
+
+
+}
